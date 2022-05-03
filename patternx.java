4efdafb0dca5f7fc4eb06b3dfead26dda7c71c59
haritha1910 <harithakumar1998@gmail.com>
2022-05-03 13:24:59 +0530

    today

---
+++ patternx.java
@@ -0,0 +1,46 @@
+/*
+//srinath
+4
+
+
+1
+1
+1
+1
+222
+222
+222
+33333
+33333
+4444444
+*/
+import java.util.*;
+class patternx
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter the n value");
+int n= sc.nextInt();
+int i,j,x,k;
+//for(i=0;i<10;i++)
+
+for(k=0;k<4;k++)
+{
+  for(j=0;j<1;j++)
+{
+System.out.print("1");
+}
+System.out.println();
+}
+
+for(k=0;k<3;k++)
+{
+  for(j=0;j<3;j++)
+{
+System.out.print("2");
+}
+System.out.println();
+}
+}
+}
